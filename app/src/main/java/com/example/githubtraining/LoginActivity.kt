182277cc2d5177674e8875fa77a6df5d7c2d445d
Reece Wanvig <rwanvig@emeris.ac.za>
2026-04-09 12:26:12 +0200

    Fixed nothing

--- app/src/main/java/com/example/githubtraining/LoginActivity.kt
+++ app/src/main/java/com/example/githubtraining/LoginActivity.kt
@@ -20,14 +20,14 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
         // Initialize Views
-        val etUsername = findViewById<EditText>(R.id.etUsername)
-        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val etUsernames = findViewById<EditText>(R.id.etUsername)
+        val etPasswords = findViewById<EditText>(R.id.etPassword)
         val btnLogin = findViewById<Button>(R.id.btnLogin)
 
         // Set Click Listener
         btnLogin.setOnClickListener {
-            val name = etUsername.text.toString().trim()
-            val password = etPassword.text.toString().trim()
+            val name = etUsernames.text.toString().trim()
+            val password = etPasswords.text.toString().trim()
 
             if (name.isEmpty() || password.isEmpty()) {
                 Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
